2a76662fb9b929ef586eadd41b7a6f094161d134
Yavorskiy Vladislav <vl.yavorskiy01@gmail.com>
2024-06-08 18:28:08 +0300

    Update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,68 @@
-const dataTimePickerEl = document.querySelector('#datetime-picker');
-console.log(dataTimePickerEl);
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  inputEl: document.querySelector('#datetime-picker'),
+  btnEl: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
+const { inputEl, btnEl, daysEl, hoursEl, minutesEl, secondsEl } = refs;
+
+let userSelectedDate;
+let intervalId;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+      btnEl.disabled = true;
+    } else if (userSelectedDate > new Date()) {
+      btnEl.disabled = false;
+    }
+  },
+};
+
+flatpickr(inputEl, options);
+
+btnEl.addEventListener('click', () => {
+  if (intervalId) clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const diff = userSelectedDate - currentTime;
+    convertMs(diff);
+  }, 1000);
+  btnEl.disabled = true;
+  inputEl.disabled = true;
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  daysEl.textContent = days.toString().padStart(2, '0');
+  hoursEl.textContent = hours.toString().padStart(2, '0');
+  minutesEl.textContent = minutes.toString().padStart(2, '0');
+  secondsEl.textContent = seconds.toString().padStart(2, '0');
+
+  return { days, hours, minutes, seconds };
+}
